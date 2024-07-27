72be180d8e781a292771fbf56957a9a8d6bdac5f
Tika Ram <tikaramchy@outlook.com>
2024-07-27 14:48:00 +0545

    migration issue fixed

--- enhancesure/Enhancesure.API/Controllers/IntervieweeController.cs
+++ enhancesure/Enhancesure.API/Controllers/IntervieweeController.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BindraSawMill.Application.Shared.Responses;
+using EnhanceSure.Application.DTOs.Interviewees;
+using EnhanceSure.Application.Features.Interviewees.Requests.Command;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnhanceSure.API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class IntervieweeController: ControllerBase {
+
+        private readonly IMediator _mediator;
+        public IntervieweeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<AppResponse<Guid>>> Post([FromBody] CreateIntervieweeDto interviewee)
+        {
+            var command = new CreateIntevieweeCommand() { CreateIntervieweeDto = interviewee };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
     }
 }
--- enhancesure/Enhancesure.Persistance/DbContexts/ApplicationDbContext.cs
+++ enhancesure/Enhancesure.Persistance/DbContexts/ApplicationDbContext.cs
@@ -23,7 +23,7 @@ namespace EnhanceSure.Persistance.DbContexts {
         public DbSet<Interviewer> Intervieweers { get; set; }
         public DbSet<InterviewSchedule> InterviewSchedules { get; set; }
 
-
+        //for Datatype information see: https://learn.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver16
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             ////  Type1
@@ -39,46 +39,46 @@ namespace EnhanceSure.Persistance.DbContexts {
             modelBuilder.Entity((EntityTypeBuilder<Interviewee> mappings) =>
             {
                 mappings.ToTable(PersistanceConstants.Tables.tblInterviewee);
-                mappings.Property(p => p.Id).IsRequired().HasColumnType("uuid");
+                mappings.Property(p => p.Id).IsRequired().HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.FirstName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.MiddleName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.LastName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.PhoneNumber).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.Address).HasColumnType("nvarchar").HasMaxLength(100);
                 mappings.Property(p => p.CreatedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.CreatedBy).HasColumnType("uuid");
-                mappings.Property(p => p.EmailAddress).HasColumnType("uuid");
+                mappings.Property(p => p.CreatedBy).HasColumnType("uniqueidentifier ");
+                mappings.Property(p => p.EmailAddress).HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.LastModifiedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.LastModifiedBy).HasColumnType("uuid");
+                mappings.Property(p => p.LastModifiedBy).HasColumnType("uniqueidentifier ");
             });
             modelBuilder.Entity((EntityTypeBuilder<Interviewer> mappings) =>
             {
                 mappings.ToTable(PersistanceConstants.Tables.tblInterviewer);
-                mappings.Property(p => p.Id).IsRequired().HasColumnType("uuid");
+                mappings.Property(p => p.Id).IsRequired().HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.FirstName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.MiddleName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.LastName).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.PhoneNumber).HasColumnType("nvarchar").HasMaxLength(10);
                 mappings.Property(p => p.Address).HasColumnType("nvarchar").HasMaxLength(100);
                 mappings.Property(p => p.CreatedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.CreatedBy).HasColumnType("uuid");
-                mappings.Property(p => p.EmailAddress).HasColumnType("uuid");
+                mappings.Property(p => p.CreatedBy).HasColumnType("uniqueidentifier ");
+                mappings.Property(p => p.EmailAddress).HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.LastModifiedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.LastModifiedBy).HasColumnType("uuid");
+                mappings.Property(p => p.LastModifiedBy).HasColumnType("uniqueidentifier ");
             });
             modelBuilder.Entity((EntityTypeBuilder<InterviewSchedule> mappings) =>
             {
                 mappings.ToTable(PersistanceConstants.Tables.tblInterviewSchedule);
-                mappings.Property(p => p.Id).IsRequired().HasColumnType("uuid");
-                mappings.Property(p => p.Interviewer).HasColumnType("uuid");
-                mappings.Property(p => p.Interviewee).HasColumnType("uuid");
+                mappings.Property(p => p.Id).IsRequired().HasColumnType("uniqueidentifier ");
+                mappings.Property(p => p.Interviewer).HasColumnType("uniqueidentifier ");
+                mappings.Property(p => p.Interviewee).HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.InterviewDateTime).HasColumnType("datetime2");
                 mappings.Property(p => p.Status).HasColumnType("int").HasMaxLength(2);
-                mappings.Property(p => p.IsDeleted).HasColumnType("bool");
+                mappings.Property(p => p.IsDeleted).HasColumnType("bit");
                 mappings.Property(p => p.CreatedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.CreatedBy).HasColumnType("uuid");
+                mappings.Property(p => p.CreatedBy).HasColumnType("uniqueidentifier ");
                 mappings.Property(p => p.LastModifiedAt).HasColumnType("datetime2");
-                mappings.Property(p => p.LastModifiedBy).HasColumnType("uuid");
+                mappings.Property(p => p.LastModifiedBy).HasColumnType("uniqueidentifier ");
 
             });
         }
